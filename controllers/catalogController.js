d5b2fc7e6b7cba63b8b6f806b90894f910b43476
GoodBoiDoggo2 <marponsie@yahoo.com>
2017-11-28 21:03:31 +0800

    catalog and item

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -3,14 +3,18 @@ angular.module('app', []);
 angular.module('app')
 .controller('catalogController', catalogController);
 
-catalogController.$inject = ['$http','$window'];
+catalogController.$inject = ['$http','$anchorScroll','$location'];
 
-function catalogController($http,$window){
+function catalogController($http,$anchorScroll,$location){
 	var vm = this;
 	vm.boi = "BOIII";
 	vm.showItem = false;
+	vm.hideReviews = true;
+	vm.showHide = 'Show'
 	vm.loaditems = loadItems;
 	vm.viewItem = viewItem;
+	vm.toggleReview = toggleReview;
+	vm.backToCatalog = backToCatalog;
 	loadItems();
 	
 	
@@ -24,7 +28,29 @@ function catalogController($http,$window){
 	 function viewItem(data){
 		 vm.showItem = true;
 		 vm.itemData = data;
-		 $window.scrollTo(0, 0);
+		 $anchorScroll();
+	 }
+	 
+	 function toggleReview(){
+		 if(vm.hideReviews){
+			 vm.showHide = 'Hide';
+			 vm.hideReviews = false;
+			 $location.hash('revHash');
+			 $anchorScroll();
+			 
+		 }
+		 else{
+			 vm.showHide = 'Show';
+			 vm.hideReviews = true;
+			 $location.hash('');
+		 }
+	 }
+	 
+	 function backToCatalog(){
+		 vm.showHide = 'Show';
+		 vm.hideReviews = true;
+		 vm.showItem = false;
+		 $location.hash('');
 	 }
 	 
 }
