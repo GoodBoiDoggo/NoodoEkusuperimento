543cfeb992373138a5e9df407ee0b6028ed3e91b
GoodBoiDoggo2 <marponsie@yahoo.com>
2017-12-07 20:59:11 +0800

    regrouped files

--- app.js
+++ app.js
@@ -4,49 +4,48 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
+//  , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
 var app = express();
-var testController = require('./controllers/testController');
+
 var mongoose = require('mongoose');
 var config = require('./config');
-var setupController = require('./controllers/setupController');
-var apiController = require('./controllers/apiController');
+
 var bodyParser = require('body-parser');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + '/app_client/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app_client')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
-app.use('/boi', express.static(path.join(__dirname, 'controllers')));
-app.use('/pic', express.static(path.join(__dirname, 'images')));
+app.use('/boi', express.static(path.join(__dirname,'app_client','controllers')));
+app.use('/pic', express.static(path.join(__dirname,'app_client','images')));
 //sdsdsfsdfsd
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-testController(app);
+//app.get('/', routes.index);
+//testController(app);
 
-
+require('./routes')(app);
 
 app.get('/users', user.list);
 
 //app.get('/bois', bois.good);
 mongoose.connect(config.getDbConnectionString());
-setupController(app);
-apiController(app);
+//setupController(app);
+//apiController(app);
 
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,34 @@
 /*
  * GET home page.
  */
+var setupController = require('../controllers/setupController');
+var apiController = require('../controllers/apiController');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+
+module.exports = function(app){
+	app.get('/',function(req,res){
+		res.render('index', { title:'Welcome to Karuteun' });
+	});
+	app.get('/api2/:id', function(req, res){
+		console.log(req.params.id);
+		res.json({firstname: 'John', lastname: 'Doe', ID: req.params.id});
+	});
+	app.get('/api2',function(req,res) {
+		console.log("BOI");
+		res.send("");
+	});
+	app.get('/api3', function(req,res){
+		console.log("YO DAWG");
+		res.json({firstname: 'Joni', lastname: 'Depth'});
+	});
+	app.get('/crud', function(req,res){
+		res.render('../views/CRUDPage.ejs');
+	});
+	app.get('/catalog', function (req, res){
+		res.render('../views/catalog.ejs');
+	});
+	
+	setupController(app);
+	apiController(app);
 };
