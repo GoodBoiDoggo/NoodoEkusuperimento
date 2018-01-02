30e58e8976ec8d621b5e3d5834e12b16474d9335
GoodBoiDoggo <yadao.meynardkhel@gmail.com>
2018-01-02 14:15:00 +0800

    Testing google smtp

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -4,14 +4,15 @@ var verification = function(email, activation) {
     console.log(email);
     console.log(activation);
     var transporter = nodemailer.createTransport({
-        service: 'yahoo',
+        service: 'Gmail',
         auth: {
-            user: 'marponsie@yahoo.com',
+            // user: 'marponsie@yahoo.com',
+            user: 'muggywarra8732@gmail.com',
             pass: 'Password8732'
         }
     });
     var mailOptions = {
-        from: 'marponsie@yahoo.com',
+        from: 'Muggy Warra',
         to: email,
         subject: 'Account Verification',
         html: '<h1>Activate your accont</h1><p>Click on the link below to activate your kariteun account.</p><br><a href="http://kariteun-shopping.mybluemix.net/api/activate/' + activation + '">Activate account</a><br><p>Thank you!</p>'
