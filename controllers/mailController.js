efaa7d0b0688f866394fd26e0de621149b977936
GoodBoiDoggo <yadao.meynardkhel@gmail.com>
2018-01-02 13:56:49 +0800

    testing yahoo smtp

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -4,14 +4,14 @@ var verification = function(email, activation) {
     console.log(email);
     console.log(activation);
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: 'yahoo',
         auth: {
-            user: 'muggywarra8732@gmail.com',
+            user: 'marponsie@yahoo.com',
             pass: 'Password8732'
         }
     });
     var mailOptions = {
-        from: 'muggywarra8732@gmail.com',
+        from: 'marponsie@yahoo.com',
         to: email,
         subject: 'Account Verification',
         html: '<h1>Activate your accont</h1><p>Click on the link below to activate your kariteun account.</p><br><a href="http://kariteun-shopping.mybluemix.net/api/activate/' + activation + '">Activate account</a><br><p>Thank you!</p>'
@@ -22,7 +22,13 @@ var verification = function(email, activation) {
             console.log(error);
         } else {
             console.log('Email sent: ' + info.response);
+            console.log('CLOSE CONNECTION.');
+            transporter.close();
         }
+
     });
+
+
+
 }
 module.exports.sendVerification = verification;
